89ffe2c8b0b89d3a3b4682ee4bb74818c6f34b10
StefanDenis08 <denis10david25@gmail.com>
2023-12-12 21:52:33 +0200

    Added more advanced UI changes to the PowerShell Script

--- start.ps1
+++ start.ps1
@@ -1,5 +1,19 @@
+# Clear the host screen
 Clear-Host
+
+# Fullscreen the console window, only if its not Visual Studio Code
+$shell = New-Object -ComObject WScript.Shell
+if ($env:TERM_PROGRAM -ne "vscode") {
+    $shell.SendKeys('{F11}')
+}
+
+# Set script path and launch the script
 $scriptPath = '.\src\app\js\app.js'
 & node $scriptPath
+
+# Pause, un-fullscreen, Clear and Exit the script 
 Pause
+if ($env:TERM_PROGRAM -ne "vscode") {
+    $shell.SendKeys('{F11}')
+}
 Clear-Host
