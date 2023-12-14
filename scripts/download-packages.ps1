34dbc4a538885d2954d486f39fd05067a5f9ae36
StefanDenis08 <denis10david25@gmail.com>
2023-12-14 20:01:28 +0200

    Installs Package Loader NPM Packages
    Functionality for API packages coming later

--- scripts/download-packages.ps1
+++ scripts/download-packages.ps1
@@ -1,1 +1,17 @@
-#TODO: create compilation file
+# Install packages for the whole Package Loader project
+
+# Clear the console
+Clear-Host
+
+# Download packages for Package Loader
+$packageLoaderSrcLocation = Join-Path $PSScriptRoot "..\src\app"
+Set-Location $packageLoaderSrcLocation
+& npm install
+
+
+# TODO: Add API package download
+
+# Exit
+if ($env:TERM_PROGRAM -eq "vscode") {
+    Set-Location -Path $PSScriptRoot
+} exit
