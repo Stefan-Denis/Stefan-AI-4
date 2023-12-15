a06b667df1b3c067ec3db15eb08fe1eb672638f6
StefanDenis08 <denis10david25@gmail.com>
2023-12-15 19:53:48 +0200

    Created Trim Method, with return types of true or false for error handling

---
+++ src/api/src/video/trim.ts
@@ -0,0 +1,29 @@
+/**
+ * @author Ștefan Denis
+ * @version 4.0.0
+ * @since 15.12.2023
+ * @description Source File that contains the function to concatenate videos together.
+ * 
+ * @project Stefan AI 4
+ */
+
+/**
+ * Imports
+ */
+import ffmpeg from 'fluent-ffmpeg'
+
+export default function trim(input: string, output: string, start: number, end: number): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        ffmpeg(input)
+            .setStartTime(start)
+            .setDuration(end - start)
+            .output(output)
+            .on('end', () => {
+                resolve(true)
+            })
+            .on('error', () => {
+                reject(false)
+            })
+            .run()
+    })
+}
