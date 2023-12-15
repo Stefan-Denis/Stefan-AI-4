37e63278ac8cf54df3674b43f60f3014d9f558bf
StefanDenis08 <denis10david25@gmail.com>
2023-12-15 20:06:56 +0200

    Method that removes audio from an input clip

---
+++ src/api/src/video/removeAudio.ts
@@ -0,0 +1,28 @@
+/**
+ * @author Ștefan Denis
+ * @version 4.0.0
+ * @since 15.12.2023
+ * @description Source File that contains the function to concatenate videos together.
+ * 
+ * @project Stefan AI 4
+ */
+
+/**
+ * Imports
+ */
+import ffmpeg from 'fluent-ffmpeg'
+
+export default function removeAudio(input: string, output: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        ffmpeg(input)
+            .noAudio()
+            .output(output)
+            .on('end', () => {
+                resolve(true)
+            })
+            .on('error', () => {
+                reject(false)
+            })
+            .run()
+    })
+}
