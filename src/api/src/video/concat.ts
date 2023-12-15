1275de7d8a03be9f2e2bd7d68de39016866f542d
StefanDenis08 <denis10david25@gmail.com>
2023-12-15 18:56:13 +0200

    Added correct documentation

--- src/api/src/video/concat.ts
+++ src/api/src/video/concat.ts
@@ -1,3 +1,15 @@
+/**
+ * @author Ștefan Denis
+ * @version 4.0.0
+ * @since 15.12.2023
+ * @description Source File that contains the function to concatenate videos together.
+ * 
+ * @project Stefan AI 4
+ */
+
+/**
+ * Imports
+ */
 import concat from 'ffmpeg-concat'
 
 export default async function concatenate(
